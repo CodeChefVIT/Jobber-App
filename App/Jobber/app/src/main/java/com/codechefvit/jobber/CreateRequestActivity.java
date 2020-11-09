e53810f9141a14528b3314713fd215ae09b7f2dd
crusher-pb <poritoshbardhan07@gmail.com>
2020-11-09 22:52:50 +0530

    Request tabs added

---
+++ App/Jobber/app/src/main/java/com/codechefvit/jobber/CreateRequestActivity.java
@@ -0,0 +1,14 @@
+package com.codechefvit.jobber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_request);
+    }
+}
--- App/Jobber/app/src/main/java/com/codechefvit/jobber/SignupActivity.java
+++ App/Jobber/app/src/main/java/com/codechefvit/jobber/SignupActivity.java
@@ -2,7 +2,9 @@ package com.codechefvit.jobber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        findViewById(R.id.signupButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignupActivity.this,RequestsActivity.class));
+            }
+        });
     }
 }
